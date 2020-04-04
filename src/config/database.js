f8edea30762526fef596faa4488d768e42c8cf50
leonardodesa <leodesa.10@gmail.com>
2020-04-04 10:38:38 -0300

    goBarber

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Retoohspw123@',
+  database: 'goBarber',
+  port: 8030,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controller/UserController';
+import ProviderController from './app/controller/ProviderController';
+import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.get('/', (req, res) => {
-  return res.json({ message:'Hello world' });
-})
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
